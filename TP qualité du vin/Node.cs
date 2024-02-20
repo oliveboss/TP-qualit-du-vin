8ebc6b311f91f9251e276c0db192ce1bc25d6295
kayembajoel92@gmail.com <kayembajoel92@gmail.com>
2024-02-20 13:31:54 -0500

    Ajout de l'interface

---
+++ TP qualité du vin/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_qualité_du_vin
+{
+    internal  class Node
+    {
+        public string Attribute { get; set; }
+        public Dictionary<string, Node> Children { get; set; }
+        public string Class { get; set; }
+        public double? SplitValue { get; set; }
+    }
+}
